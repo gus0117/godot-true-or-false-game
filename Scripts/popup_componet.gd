c609cda60ab7f66b3d6ab924a760e633e328792e
Gustavo Ordoñez <gustavoalberto117@gmail.com>
2024-05-19 01:21:04 -0300

    Add logic to popup component

--- Scripts/popup_componet.gd
+++ Scripts/popup_componet.gd
@@ -1,12 +1,37 @@
 extends Control
 
+# References
 @onready var label = $Label
+@onready var bg_color = $BgColor
+@onready var animation_player = $AnimationPlayer
 
-# Called when the node enters the scene tree for the first time.
+# Color Settings
+@export var failColor : Color
+@export var successColor: Color
+
+#Text Settings
+@export var failText := "Incorrect"
+@export var successText := "Correct"
+
+# Signals
+signal on_next # Signal to pass to next question
+
 func _ready():
-	pass # Replace with function body.
+	ShowIncorrectMsg()
 
+func ShowCorrectMsg() -> void:
+	bg_color.material.set_shader_parameter("color", successColor)
+	label.text = successText
+	animation_player.play("show")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func ShowIncorrectMsg() -> void:
+	bg_color.material.set_shader_parameter("color", failColor)
+	label.text = failText
+	animation_player.play("show")
+
+func HidePopup() -> void:
+	animation_player.play("hide")
+
+func _on_next_btn_pressed():
+	on_next.emit()
+	HidePopup()
