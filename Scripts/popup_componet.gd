919845d58237cbc60acf58e088bac6d9c0c00ac7
Gustavo Ordoñez <gustavoalberto117@gmail.com>
2024-05-26 11:26:27 -0300

    Add logic to skip question in popup component

--- Scripts/popup_componet.gd
+++ Scripts/popup_componet.gd
@@ -8,6 +8,7 @@ class_name PopupComponent
 @onready var animation_player = $AnimationPlayer
 @onready var correct_icon = $Icons/Correct
 @onready var incorrect_icon = $Icons/Incorrect
+@onready var skip_icon = $Icons/Skip
 
 #References
 var questionManager : QuestionManager
@@ -15,10 +16,12 @@ var questionManager : QuestionManager
 # Color Settings
 @export var failColor : Color
 @export var successColor: Color
+@export var skipColor: Color
 
 #Text Settings
 @export var failText := "Incorrect"
 @export var successText := "Correct"
+@export var skipText := "Skipped"
 
 # Signals
 signal on_next # Signal to pass to next question
@@ -29,6 +32,7 @@ func _ready():
 		questionManager.answer_checked.connect(showPopup)
 	correct_icon.visible = false
 	incorrect_icon.visible = false
+	skip_icon.visible = false
 
 func showPopup(value: int) -> void:
 	if value < 0:
@@ -49,11 +53,17 @@ func ShowIncorrectMsg() -> void:
 	incorrect_icon.visible = true
 	animation_player.play("show")
 	
+func ShowSkipMsg() -> void:
+	bg_color.material.set_shader_parameter("color", skipColor)
+	label.text = skipText
+	skip_icon.visible = true
+	animation_player.play("show")
 
 func HidePopup() -> void:
 	animation_player.play("hide")
 	correct_icon.visible = false
 	incorrect_icon.visible = false
+	skip_icon.visible = false
 
 func _on_next_btn_pressed():
 	on_next.emit()
